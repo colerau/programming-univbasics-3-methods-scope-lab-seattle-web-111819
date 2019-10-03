27f030ba75e1ac762f5e5a1fea534ec52596fa9e
Cole Rau <colevrau@gmail.com>
2019-10-03 02:40:08 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
-#def mario
-  #
- # puts phrase
-#end
+def mario
+  phrase = "It's-a me, Mario!"
+  puts phrase
+end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
